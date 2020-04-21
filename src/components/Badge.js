88b0d46b998f1503683250e3a445b5a1ffc01703
Cocreacion 3 <cocreacion3@hgm.com>
2020-04-21 13:48:21 -0500

    Cambios Generales proyecto

---
+++ src/components/Badge.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Badge extends React.Component{
+render (){
+return (
+
+<h1> Hello Omar PinoF</h1>
+
+)
+
+
+}
+
+}
+
+export default Badge;
+
+/* 
+
+<div>
+  <div>
+    Titulo de encabezado
+  </div>
+ <div>
+   <img src= >   </img>
+   <h1>Omar <br/> Pino </h1>
+
+ </div>
+  <div>
+<p>Desarrollador WEB Front END y BACK END</p>
+<h3>Twitter</h3>
+
+ </div>
+
+</div> */
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Badge from './components/Badge'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+/* const element = (
+<div>
+<h1>Hola, Soy Omar Pino</h1>
+<p>Soy Ingeniero Mecatrónico</p>
+</div>
+
+
+); */
+
+const container = document.getElementById('app');
+
+ReactDOM.render (<Badge/>,container);
